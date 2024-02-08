90f10eeb9b82e716fb4a1ff7760b11b9377c3515
leeoocca <leeoocca@gmail.com>
2024-02-08 16:35:02 +0100

    add support for multiple cursuses

---
+++ config.js
@@ -0,0 +1,5 @@
+const campuses = [{ id: 53, name: "Vienna" }];
+
+const cursuses = [{ id: 21, name: "42cursus" }];
+
+module.exports = { campuses, cursuses };
--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-const express = require('express');
-const ical = require('ical-generator');
-const dotenv = require('dotenv').config();
-const { ClientCredentials } = require('simple-oauth2');
-const { getCampusCalendar } = require('./calendar.js');
-const campuses = require('./campus.js');
+const express = require("express");
+const ical = require("ical-generator");
+const dotenv = require("dotenv").config();
+const { ClientCredentials } = require("simple-oauth2");
+const { getCampusCalendar } = require("./calendar.js");
+const { campuses, cursuses } = require("./config.js");
 
 const config = {
 	client: {
 		id: process.env.CLIENT_ID,
-		secret: process.env.CLIENT_SECRET 
+		secret: process.env.CLIENT_SECRET,
 	},
 	auth: {
-		tokenHost: 'https://api.intra.42.fr',
-		tokenPath: '/oauth/token',
-		authorizePath: '/oauth/authorize'
-	}
+		tokenHost: "https://api.intra.42.fr",
+		tokenPath: "/oauth/token",
+		authorizePath: "/oauth/authorize",
+	},
 };
 const client = new ClientCredentials(config);
 
 let calendars = {};
+
 for (const campus of campuses) {
-	calendars[campus.id] = ical({name: `42 Events | ${campus.name}`});
+	calendars[campus.id] = {};
+	for (const cursus of cursuses) {
+		calendars[campus.id][cursus.id] = ical({
+			name: `Intra ${campus.name} | ${cursus.name}`,
+		});
+	}
 }
+
 campuses.forEach((campus, index) => {
-	setTimeout(() => {
-		getCampusCalendar(client, calendars[campus.id], campus);
-		setInterval(getCampusCalendar, 10 * 60 * 1000, client, calendars[campus.id], campus);
-	}, 500 * index);
+	cursuses.forEach((cursus, index) => {
+		setTimeout(() => {
+			getCampusCalendar(
+				client,
+				calendars[campus.id][cursus.id],
+				campus,
+				cursus
+			);
+			setInterval(
+				getCampusCalendar,
+				10 * 60 * 1000,
+				client,
+				calendars[campus.id][cursus.id],
+				campus,
+				cursus
+			);
+		}, 500 * index);
+	});
 });
 
 const app = express();
 
-const default_campus = '1';
-app.get('/', async (req, res) => {
-	if (!calendars.hasOwnProperty(default_campus)) return res.status(404).send('The specified campus does not exist...');
-	return calendars[default_campus].serve(res)
+app.get("/", async (req, res) => {
+	return res.status(200).send("42cal");
 });
 
-app.get('/campus/:id', async (req, res) => {
-	if (!calendars.hasOwnProperty(req.params.id)) return res.status(404).send('The specified campus does not exist...');
-	return calendars[req.params.id].serve(res)
+app.get("/campus/:campus_id/cursus/:cursus_id", async (req, res) => {
+	if (
+		!calendars.hasOwnProperty(req.params.campus_id) ||
+		!calendars[req.params.campus_id].hasOwnProperty(req.params.cursus_id)
+	)
+		return res.status(404).send("Calendar does not exist...");
+	return calendars[req.params.campus_id][req.params.cursus_id].serve(res);
 });
 
 app.listen(3000, () => {
-	console.log('Running on http://localhost:3000');
+	console.log("Running on http://localhost:3000");
 });
